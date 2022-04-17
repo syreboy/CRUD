85fc770a7a33fbe96dc45e414771e0ab20063c90
syreboy <80834548+syreboy@users.noreply.github.com>
2022-04-17 14:19:09 +0700

    Add project files.

---
+++ CRUD/WebForm1.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CRUD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <table>
+        <tr>
+            <td colspan="2">
+                <h1>Insertion</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>Employee ID</td>
+            <td>:</td>
+            <td>
+                <asp:TextBox ID="txtEmpID" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>Employee First Name</td>
+            <td>:</td>
+            <td>
+                <asp:TextBox ID="txtEmpFname" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <asp:Button ID="btnInsertion" runat="server" Text="Insert" OnClick="btnInsertion_Click" Style="width: 48px" />
+                <asp:Button ID="btnUpdation" runat="server" Text="Update" OnClick="btnUpdation_Click" />
+                <asp:Label ID="lblmsg" runat="server" ForeColor="Red"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td>
+                <h1>Selection</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="btnSelect" runat="server" Text="Select All Data" OnClick="btnSelect_Click" />
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+            </td>
+        </tr>
+    </table>
+    <table>
+        <tr>
+            <td colspan="3">
+                <h1>Deletion</h1>
+            </td>
+        </tr>
+        <tr>
+            <td>Employee ID</td>
+            <td>:</td>
+            <td>
+                <asp:TextBox ID="txtEmployeeID" runat="server"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+                <asp:Label ID="lblmessage" runat="server" ForeColor="Red"></asp:Label>
+            </td>
+        </tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ CRUD/WebForm1.aspx.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+using System.Configuration;
+using Npgsql;
+
+namespace CRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void btnInsertion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /* Insertion After Validations*/
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost ; Port=5432 ; Database=postgres ;User Id=postgres; Password=12345"))
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Insert into pabrikan(namapabrikan) values(@name)";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@name", txtEmpFname.Text));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtEmpFname.Text = "";
+                    txtEmpID.Text = "";
+                    lblmsg.Text = "Data Has been Saved";
+                }
+            }
+            catch (Exception ex) { }
+        }
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            try /* Select After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost ; Port=5432 ; Database=postgres ;User Id=postgres; Password=12345"))
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Select * from pabrikan";
+                    cmd.CommandType = CommandType.Text;
+                    NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    cmd.Dispose();
+                    connection.Close();
+
+                    GridView1.DataSource = dt;
+                    GridView1.DataBind();
+                }
+            }
+            catch (Exception ex) { }
+        }
+        protected void btnUpdation_Click(object sender, EventArgs e)
+        {
+            try /* Updation After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost ; Port=5432 ; Database=postgres ;User Id=postgres; Password=12345"))
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "update pabrikan set namapabrikan=@name where idpabrikan=@ID";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@ID", int.Parse(txtEmpID.Text)));
+                    cmd.Parameters.Add(new NpgsqlParameter("@name", txtEmpFname.Text));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtEmpFname.Text = "";
+                    txtEmpID.Text = "";
+                    lblmsg.Text = "Data Has been Updated";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+        }
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try /* Deletion After Validations*/
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection("Server=localhost ; Port=5432 ; Database=postgres ;User Id=postgres; Password=12345"))
+                {
+                    connection.ConnectionString = ConfigurationManager.ConnectionStrings["constr"].ToString();
+                    connection.Open();
+                    NpgsqlCommand cmd = new NpgsqlCommand();
+                    cmd.Connection = connection;
+                    cmd.CommandText = "Delete from pabrikan where idpabrikan=@ID";
+                    cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add(new NpgsqlParameter("@ID", int.Parse(txtEmployeeID.Text)));
+                    cmd.ExecuteNonQuery();
+                    cmd.Dispose();
+                    connection.Close();
+                    txtEmployeeID.Text = "";
+                    lblmessage.Text = "Data Has been Deleted";
+                }
+            }
+            catch (Exception ex) { }
+        }
+    }
+}
